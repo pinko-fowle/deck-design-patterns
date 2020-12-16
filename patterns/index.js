595c88ef2c029846debee01b3e26f94422814a33
Matthew Fowle <mfowle@socialtables.com>
2020-12-16 13:17:23 -0500

    History & clean up

--- patterns/index.js
+++ patterns/index.js
@@ -86,20 +86,14 @@ int main(string[] args){
 const formidableLogo = "/cover-gof.png";
 
 const index = [
-	// 0
-	"Foreward",
-	"What is a Design Pattern?",
-	"What? Elements of Design Patterns",
-	"Checking out some Design Patterns",
-	"What? Invariance of Design Patterns",
-	"Where did Design Patterns come from?",
-	"Some more patterns",
+	"Foreward", // 0
+	"What is a Design Pattern?", // 1
+	"What? Elements of Design Patterns", // 2
+	"In depth with Observer Pattern", // 3
+	"Some more MVC patterns", //4 
+	"Where did Design Patterns come from?", // 5
+	"What kinds of design patterns are there?", // 6
 	"What do we hope to gain from design patterns?",
-	"Where did design patterns come from?",
-	"What kinds of design patterns are there?",
-	"Creation Patterns",
-	"Structural Patterns",
-	"Behavioral Patterns"
 ]
 
 const snake = n => n.toLowerCase().replaceAll(/ /g, "_").replaceAll(/[?]/g, "")
@@ -280,7 +274,7 @@ const Presentation = () => (
 		</Slide>
 		<Slide>
 			<Text></Text>
-			<Text>And that is the sense what a design pattern is</Text>
+			<Text>This captures the core ideas of a design pattern</Text>
 			<Appear elementNum={0}>
 				<Text>An <b>identified</b>, <b>resuable</b> <b>solution</b> to commonly re-ocurring <b>problems</b> or situations</Text>
 			</Appear>
@@ -330,9 +324,10 @@ const Presentation = () => (
 	
 		{IndexSlide({n: 3})}
 		<Slide>
+			<Sub>Looking at our first official pattern</Sub>
 			<Quote margin="8px 16px">{gof.mvcExample.invarianceObserver}</Quote>
 			<Text>
-				We'll come back to the topic of what makes a good design pattern latter, once we're a a little more familiar with some other patterns. For now:
+				We'll come back to the topic of what makes a good design pattern latter, once we're a little more familiar with some other patterns. For now:
 				<br/>
 				Let's go check out our first official <i>Design Patterns</i> pattern: <b>Observer Pattern</b>.
 			</Text>
@@ -402,6 +397,16 @@ const Presentation = () => (
 		</Slide>
 		<Slide>
 			<Heading>What have we seen about Design Patterns?</Heading>
+			<Text>
+				We've found a good overview of a <b>problem</b>, and a <b>solution</b>, which is named
+				<br/>
+				It's given us framing to talk about structure, action, implementations, and consequences
+				<br/>
+				It's linked to other related patterns
+			</Text>
+		</Slide>
+		<Slide>
+			<Heading>What have we seen about Design Patterns?</Heading>
 			<Text margin="0px 16px">
 				<i>Design Patterns</i> showed a specific implementation Observer Pattern, but also talked to:
 			</Text>
@@ -419,6 +424,8 @@ const Presentation = () => (
 			</Text>
 			<Quote>
 				{pl.elements.imposeNothing}
+				<br/>
+				- Christopher Alexander &al, <i>A Pattern Language</i>
 			</Quote>
 		</Slide>
 
@@ -448,15 +455,32 @@ const Presentation = () => (
 			<Notes>Show off diagrams from book</Notes>
 		</Slide>
 
-		{IndexSlide({n: 4})}
-		<Slide>
-			<Heading>Invariance, generalness</Heading>
-		</Slide>
+		{IndexSlide({n: 5})}
 
-		{/* patterns you know: prototype, singleton,   */}
+		{/* patterns you (the audience) know: prototype, singleton,   */}
 		{/* favorite patterns:	*/}
 
 		<Slide>
+			<Heading>A start for design patterns</Heading>
+			<Text>We've been looking for patterns informally... forever?</Text>
+			<Text>But...</Text>
+		</Slide>
+		<Slide>
+			<Heading>Ward Cunningham & Kent Beck, 1987</Heading>
+			<Quote>
+				Ward cautioned against requiring too much programming at, what he termed,
+				'the high level of wizards.' He pointed out that a written 'pattern language'
+				can significantly improve the selection and application of abstractions. He
+				proposed a 'radical shift in the burden of design and implementation' basing the
+				new methodology on an adaptation of Christopher Alexander's work in pattern
+				languages and that programming-oriented pattern languages developed at Tektronix
+				has significantly aided their software development efforts.
+				<br/>
+				- Reid Smith, Panel On Design Methodology, OOPSLA 1987
+			</Quote>
+		</Slide>
+		<Slide>
+			<Sub>Christopher Alexander</Sub>
 			<FlexBox height="100%">
 				<Appear elementNum={1} id="timeless">
 					<Heading fontSize="h3">The Timeless Way of Building</Heading>
@@ -469,180 +493,9 @@ const Presentation = () => (
 			</FlexBox>
 		</Slide>
 
+		{IndexSlide({n: 6})}
+		{IndexSlide({n: 7})}
 		{TakeAwaySlide()}
-
-
-		<Slide
-			backgroundColor="tertiary"
-			backgroundImage="url(https://github.com/FormidableLabs/dogs/blob/main/beau.jpg?raw=true)"
-			backgroundOpacity={0.5}
-		>
-			<Heading>Custom Backgrounds</Heading>
-			<UnorderedList>
-				<ListItem>
-					<CodeSpan>backgroundColor</CodeSpan>
-				</ListItem>
-				<ListItem>
-					<CodeSpan>backgroundImage</CodeSpan>
-				</ListItem>
-				<ListItem>
-					<CodeSpan>backgroundOpacity</CodeSpan>
-				</ListItem>
-				<ListItem>
-					<CodeSpan>backgroundSize</CodeSpan>
-				</ListItem>
-				<ListItem>
-					<CodeSpan>backgroundPosition</CodeSpan>
-				</ListItem>
-				<ListItem>
-					<CodeSpan>backgroundRepeat</CodeSpan>
-				</ListItem>
-			</UnorderedList>
-		</Slide>
-		<Slide transitionEffect="slide">
-			<Heading>Code Blocks</Heading>
-			<Stepper
-				defaultValue={[]}
-				values={[
-					[1, 1],
-					[23, 25],
-					[40, 42]
-				]}
-			>
-				{(value, step) => (
-					<Box position="relative">
-						<CodePane
-							highlightStart={value[0]}
-							highlightEnd={value[1]}
-							fontSize={18}
-							language="cpp"
-							autoFillHeight
-						>
-							{cppCodeBlock}
-						</CodePane>
-
-						<Box
-							position="absolute"
-							bottom="0rem"
-							left="0rem"
-							right="0rem"
-							bg="black"
-						>
-							{/* This notes container won't appear for step 0 */}
-
-							{step === 1 && (
-								<Text fontSize="1.5rem" margin="0rem">
-									This is a note!
-								</Text>
-							)}
-
-							{step === 2 && (
-								<Text fontSize="1.5rem" margin="0rem">
-									You can use the stepper state to render whatever you like as
-									you step through the code.
-								</Text>
-							)}
-						</Box>
-					</Box>
-				)}
-			</Stepper>
-			<Text>
-				Code Blocks now auto size and scroll when there is an overflow of
-				content! They also auto-wrap longer lines.
-			</Text>
-		</Slide>
-		<Slide>
-			<Heading>Animated Elements</Heading>
-			<OrderedList>
-				<Appear elementNum={0}>
-					<ListItem>Elements can animate in!</ListItem>
-				</Appear>
-				<Appear elementNum={2}>
-					<ListItem>
-						Just identify the order with the prop{' '}
-						<CodeSpan>elementNum</CodeSpan>!
-					</ListItem>
-				</Appear>
-				<Appear elementNum={1}>
-					<ListItem>Out of order</ListItem>
-				</Appear>
-			</OrderedList>
-		</Slide>
-		<Slide>
-			<FlexBox>
-				<Text>These</Text>
-				<Text>Text</Text>
-				<Text color="secondary">Items</Text>
-				<Text fontWeight="bold">Flex</Text>
-			</FlexBox>
-			<Grid gridTemplateColumns="1fr 2fr" gridColumnGap={15}>
-				<Box backgroundColor="primary">
-					<Text color="secondary">Single-size Grid Item</Text>
-				</Box>
-				<Box backgroundColor="secondary">
-					<Text>Double-size Grid Item</Text>
-				</Box>
-			</Grid>
-			<Grid
-				gridTemplateColumns="1fr 1fr 1fr"
-				gridTemplateRows="1fr 1fr 1fr"
-				alignItems="center"
-				justifyContent="center"
-				gridRowGap={1}
-			>
-				{Array(9)
-					.fill('')
-					.map((_, index) => (
-						<FlexBox paddingTop={0} key={`formidable-logo-${index}`} flex={1}>
-							<Image src={formidableLogo} width={100} />
-						</FlexBox>
-					))}
-			</Grid>
-		</Slide>
-		<Slide>
-			<Markdown>
-				{`
-					# Layout Tables in Markdown
-
-					| Browser				 | Supported | Versions |
-					|-----------------|-----------|----------|
-					| Chrome					| Yes			 | Last 2	 |
-					| Firefox				 | Yes			 | Last 2	 |
-					| Opera					 | Yes			 | Last 2	 |
-					| Edge (EdgeHTML) | No				|					|
-					| IE 11					 | No				|					|
-				`}
-			</Markdown>
-		</Slide>
-		<Markdown containsSlides>
-			{`
-				### Even write multiple slides in Markdown
-				> Wonderfully formatted quotes
-
-				1. Even create
-				2. Lists in Markdown
-
-
-				- Or Unordered Lists
-				- Too!!
-				Notes: These are notes
-				---
-				### This slide was also generated in Markdown!
-
-				\`\`\`jsx
-				const evenCooler = "is that you can do code in Markdown";
-				// You can even specify the syntax type!
-				\`\`\`
-
-				### A slide can have multiple code blocks too.
-
-				\`\`\`c
-				char[] someString = "Popular languages like C too!";
-				\`\`\`
-
-				Notes: These are more notes
-			`}
-		</Markdown>
 	</Deck>
 );
 
